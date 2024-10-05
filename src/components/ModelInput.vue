15d3ac963a3cc813e755513bc88b08aaafda3779
frytaz <frytaz@gmail.com>
2024-10-05 21:00:28 +0200

    fix: model load

--- src/components/ModelInput.vue
+++ src/components/ModelInput.vue
@@ -5,7 +5,7 @@
   </InputGroup>
 </template>
 <script setup lang="ts">
-const modelInputUrl = ref('https://xeovision.io/project/227c848e-ae98-473a-ab15-a0111fe548db');
+const modelInputUrl = ref('https://xeovision.io/project/60ef6841-8d28-4575-bb6e-c5e88c1ad9ed');
 const { $trpc } = useNuxtApp();
 
 const router = useRouter();
