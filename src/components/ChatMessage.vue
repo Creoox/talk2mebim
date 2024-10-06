07170a73bb68fa787acdaef6d399d6e18e6c8957
DhivinX <d.szaradowski@gmail.com>
2024-10-06 11:56:33 +0200

    feat: chat styles

--- src/components/ChatMessage.vue
+++ src/components/ChatMessage.vue
@@ -4,17 +4,27 @@
       {{ props.who }}
     </template>
 
-    <p class="m-0">
-      {{ props.text }}
-    </p>
+    <p class="m-0" v-html="marked.parse(props.text)"></p>
   </Panel>
 </template>
 
+<style scoped>
+.p-panel {
+  background-color: v-bind(color);
+}
+</style>
+
 <script setup lang="ts">
+import { marked } from 'marked';
+
 interface Props {
   text: string;
   who: string;
 }
+
+const color = computed(() => {
+  return props.who === 'ai' ? '#474747' : '#2b4053';
+});
 
 const props = defineProps<Props>();
 </script>
